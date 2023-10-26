f2f16457f4f7ddfad4447c030e5a4349649b825a
MagiCaps <a.izard@hotmail.com>
2023-10-26 10:49:24 -0400

    Création des fonctions dans le .c

---
+++ Mini AES.c
@@ -0,0 +1,21 @@
+#include "Mini AES.h"
+
+void subBytes(Message *message){
+
+}
+
+void shiftRows(Message *message){
+
+}
+
+void mixColumns(Message *message){
+
+}
+
+void addRoundKey(Message *message, Message *roundKey){
+
+}
+
+void encrypt(Message *message, Message *roundKey, int round){
+    
+}

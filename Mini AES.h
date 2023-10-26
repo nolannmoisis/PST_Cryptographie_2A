ac788a946c9c0d5c8393a4af2caeb84921e46d23
MagiCaps <a.izard@hotmail.com>
2023-10-26 10:46:36 -0400

    Création des fonctions

--- Mini AES.h
+++ Mini AES.h
@@ -5,3 +5,39 @@
 #include"settings.h"
 
 #endif
+
+/// @brief 
+typedef unsigned char byte;
+
+/// @brief 
+typedef struct Message_s
+{
+    /// @brief 
+    unsigned int size;
+
+    /// @brief 
+    byte *tab;
+} Message;
+
+/// @brief 
+/// @param message 
+void subBytes(Message *message);
+
+/// @brief 
+/// @param message 
+void shiftRows(Message *message);
+
+/// @brief 
+/// @param message 
+void mixColumns(Message *message);
+
+/// @brief 
+/// @param message 
+/// @param roundKey 
+void addRoundKey(Message *message, Message *roundKey);
+
+/// @brief 
+/// @param message 
+/// @param roundKey 
+/// @param round 
+void encrypt(Message *message, Message *roundKey, int round);

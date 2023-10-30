642f93dc3b6696032a94f6c0b2bb5efc924f4ab4
Nolann <nolannmoisis@gmail.com>
2023-10-30 13:14:17 +0100

    NULL
    NULL

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char **argv)
 {
-    Message *message = messageCreate("./instance1.txt");
+    Message *message = messageCreate("./instance2.txt");
     messagePrint(message);
     //printf("[%d] [%d]\n[%d] [%d]\n\n", (int)message->tab[0], (int)message->tab[2], (int)message->tab[1], (int)message->tab[3]);
     //subBytes(message);

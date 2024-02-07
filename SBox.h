51e60bbef9256d2fd28edafba8a03d15b3263a2a
Nicolas2027g <119615186+Nicolas2027g@users.noreply.github.com>
2024-02-07 17:44:59 +0100

    send

--- SBox.h
+++ SBox.h
@@ -1,9 +1,12 @@
-#include "settings.h"
-#include "Mini AES.h"
 #ifndef _SBOX_H_
 #define _SBOX_H_
 
 #include "settings.h"
 #include "Mini AES.h"
+
+byte **SBox_DifferentialProbability(void);
+
+byte **SBox_LinearProbability(void);
+
 
 #endif

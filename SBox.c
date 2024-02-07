57bb6fac8e017cb574a8832d300c58a435ae5b1e
Nicolas2027g <119615186+Nicolas2027g@users.noreply.github.com>
2024-02-07 17:54:15 +0100

    func

--- SBox.c
+++ SBox.c
@@ -1,1 +1,22 @@
 #include "SBox.h"
+
+byte **SBox_DifferentialProbability(void){
+
+    byte **DP = (byte**)calloc(S_BOX_SIZE, sizeof(byte*));
+    byte *buffer = (byte*)calloc(S_BOX_SIZE, sizeof(byte));
+
+    for(int i = 0; i < S_BOX_SIZE; i++){
+
+        DP[i] = (byte*)calloc(S_BOX_SIZE, sizeof(byte));
+
+        for(int j = 0; j < S_BOX_SIZE; j++){
+            
+        }
+    }
+
+    free(buffer);
+}
+
+byte **SBox_LinearProbability(void){
+
+}
--- SBox.h
+++ SBox.h
@@ -4,6 +4,11 @@
 #include "settings.h"
 #include "Mini AES.h"
 
+const int S_BOX_SIZE = 16;
+
+/// @brief Fonction Remplissant une matrice
+/// @param  
+/// @return La matrice des DP de la S-box
 byte **SBox_DifferentialProbability(void);
 
 byte **SBox_LinearProbability(void);

3dd15f4cc376b7e2931d98f930b9888cc854068d
Peluvre <daubigne@et.esiea.fr>
2024-02-22 17:37:45 +0100

    ToyCipher

---
+++ ToyCipher.h
@@ -0,0 +1,12 @@
+#ifndef _MINI_AES_H_
+#define _MINI_AES_H_
+
+//inclusion des settings
+#include"settings.h"
+
+/// @brief Permet de transformer unsigned char en terme byte
+typedef unsigned char byte;
+
+
+
+#endif

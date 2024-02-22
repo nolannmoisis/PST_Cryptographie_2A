1e0d22319ad39e823262612945408e630c194e6e
Nicolas2027g <119615186+Nicolas2027g@users.noreply.github.com>
2024-02-22 19:02:01 +0100

    changement c mtn

--- ToyCipher.h
+++ ToyCipher.h
@@ -4,13 +4,11 @@
 //inclusion des settings
 #include"settings.h"
 
-/// @brief Permet de transformer unsigned char en terme byte
-typedef unsigned char byte;
-
 extern const byte subBytesTab[16];
 
 byte **ToyCipherKey_create(byte a, byte b, byte c, byte d);
 void ToyCipher_delete(byte **key);
 int ToyCipherKey_permutation(byte *tab);
+void ToyCipher_subBytes(byte *tab);
 
 #endif

c26c46085effb80b88a5477f614b61d525f4924d
Peluvre <daubigne@et.esiea.fr>
2024-06-10 13:18:26 +0200

    push de l'espace

---
+++ AES_Biclique/settings.h
@@ -0,0 +1,17 @@
+#ifndef _SETTINGS_H_
+#define _SETTINGS_H_
+
+//Définition des données
+/// @brief Permet de transformer unsigned char en terme byte
+typedef unsigned char byte;
+
+//Inclusion des librairies de base
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<time.h>
+#include<math.h>
+#include<assert.h>
+
+
+#endif
